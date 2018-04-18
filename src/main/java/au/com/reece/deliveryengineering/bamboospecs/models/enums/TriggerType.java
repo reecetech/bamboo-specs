fa2909de2580814403d43444cf38930ced1857a6
Richard Jones <richard.jones@reece.com.au>
2018-04-19 09:31:54 +1000

    Add scheduled triggers.

--- src/main/java/au/com/reece/deliveryengineering/bamboospecs/models/enums/TriggerType.java
+++ src/main/java/au/com/reece/deliveryengineering/bamboospecs/models/enums/TriggerType.java
@@ -1,5 +1,5 @@
 package au.com.reece.deliveryengineering.bamboospecs.models.enums;
 
 public enum TriggerType {
-    AFTER_STASH_COMMIT, AFTER_SUCCESSFUL_BUILD_PLAN
+    AFTER_STASH_COMMIT, AFTER_SUCCESSFUL_BUILD_PLAN, SCHEDULED
 }
