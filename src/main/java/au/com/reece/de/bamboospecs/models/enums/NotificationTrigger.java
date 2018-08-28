000fa58cd37af7bb54e99eb6075d1a8dbb638893
karunasa <asanga.karunasundarage@reece.com.au>
2018-08-29 07:08:47 +1000

    Supporting the "Change of Build Status" Bamboo notification event
    While it's nice to get notified about failures, it's equally useful to get notifications on subsequent build 'fixes' as well.
    Ideally we should support 'Failed Builds And First Successful' event, but Bamboo Specs does not seem to support it at the moment.

--- src/main/java/au/com/reece/de/bamboospecs/models/enums/NotificationTrigger.java
+++ src/main/java/au/com/reece/de/bamboospecs/models/enums/NotificationTrigger.java
@@ -3,6 +3,7 @@ package au.com.reece.de.bamboospecs.models.enums;
 public enum NotificationTrigger {
     PLAN_COMPLETED,
     PLAN_FAILED,
+    STATUS_CHANGED,
     DEPLOYMENT_FAILED,
     DEPLOYMENT_FINISHED,
     DEPLOYMENT_STARTED_AND_FINISHED
